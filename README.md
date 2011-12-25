3f2516d06a08193289619715655f5c07a14a97c6
narkisr <narkisr@gmail.com>
2011-12-25 20:42:21 +0200

    importing code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gradle application plugin extended
+
+This is core Gradle application plugin extracted from the Gradle project, the main goal is to extend the core plugin to match specific needs such as setting the zip root entry name.
---
+++ src/test/groovy/org/gradle/api/plugins/ApplicationPluginTest.groovy
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.api.plugins
+
+import org.gradle.api.Project
+import org.gradle.api.tasks.application.CreateStartScripts
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.bundling.Zip
+import org.gradle.util.HelperUtil
+import org.gradle.util.Matchers
+import spock.lang.Specification
+import org.gradle.api.tasks.Sync
+
+class ApplicationPluginTest extends Specification {
+    private final Project project = HelperUtil.createRootProject();
+    private final ApplicationPlugin plugin = new ApplicationPlugin();
+
+    public void appliesJavaPluginAndAddsConventionObjectWithDefaultValues() {
+        when:
+        plugin.apply(project)
+
+        then:
+        project.plugins.hasPlugin(JavaPlugin.class)
+        project.convention.getPlugin(ApplicationPluginConvention.class) != null
+        project.applicationName == project.name
+        project.mainClassName == null
+    }
+
+    public void addsRunTasksToProject() {
+        when:
+        plugin.apply(project)
+
+        then:
+        def task = project.tasks[ApplicationPlugin.TASK_RUN_NAME]
+        task instanceof JavaExec
+        task.classpath == project.sourceSets[SourceSet.MAIN_SOURCE_SET_NAME].runtimeClasspath
+        task Matchers.dependsOn('classes')
+    }
+
+    public void addsCreateStartScriptsTaskToProject() {
+        when:
+        plugin.apply(project)
+
+        then:
+        def task = project.tasks[ApplicationPlugin.TASK_START_SCRIPTS_NAME]
+        task instanceof CreateStartScripts
+        task.applicationName == project.applicationName
+        task.outputDir == project.file('build/scripts')
+    }
+
+    public void addsInstallTaskToProjectWithDefaultTarget() {
+        when:
+        plugin.apply(project)
+
+        then:
+        def task = project.tasks[ApplicationPlugin.TASK_INSTALL_NAME]
+        task instanceof Sync
+        task.destinationDir == project.file("build/install/${project.applicationName}")
+    }
+
+    public void addsDistZipTaskToProject() {
+        when:
+        plugin.apply(project)
+
+        then:
+        def task = project.tasks[ApplicationPlugin.TASK_DIST_ZIP_NAME]
+        task instanceof Zip
+        task.archiveName == "${project.applicationName}.zip"
+    }
+
+    public void canChangeApplicationName() {
+        when:
+        plugin.apply(project)
+        project.applicationName = "SuperApp";
+
+        then:
+        def startScriptsTask = project.tasks[ApplicationPlugin.TASK_START_SCRIPTS_NAME]
+        startScriptsTask.applicationName == 'SuperApp'
+
+        def installTest = project.tasks[ApplicationPlugin.TASK_INSTALL_NAME]
+        installTest.destinationDir == project.file("build/install/SuperApp")
+
+        def distZipTask = project.tasks[ApplicationPlugin.TASK_DIST_ZIP_NAME]
+        distZipTask.archiveName == "SuperApp.zip"
+    }
+    
+    public void setMainClassNameSetsMainInRunTask() {
+        when:
+        plugin.apply(project)
+        project.mainClassName = "Acme";
+
+        then:
+        def run = project.tasks[ApplicationPlugin.TASK_RUN_NAME]
+        run.main == "Acme"
+    }
+
+    public void setMainClassNameSetsMainClassNameInCreateStartScriptsTask() {
+        when:
+        plugin.apply(project);
+        project.mainClassName = "Acme"
+
+        then:
+        def startScripts = project.tasks[ApplicationPlugin.TASK_START_SCRIPTS_NAME]
+        startScripts.mainClassName == "Acme"
+    }
+}
